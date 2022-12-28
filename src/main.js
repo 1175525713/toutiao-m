96e7d1f0e9451e213d23d866953d7bb177aec4c9
wan <1175525713@qq.com>
2022-12-28 18:20:21 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import './styles/index.less'
+
+Vue.use(VueRouter)
+
+const routers=[]
+
+const router=new VueRouter({
+  routers
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
